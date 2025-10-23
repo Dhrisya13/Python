954401f3cca23218c3a3334ee27fe8d36bd31394
Dhrisya13 <dhrisyajithesh@gmail.com>
2025-10-23 10:49:09 +0530

    completed

---
+++ modules/rec_module.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+from math import pi
+def area_r(l,b):
+    print("area of the rectangle:",l*b)
+def area_c(r):
+    print("area of the circle:",(pi*r*r)
+def perimeter_r(l,b):
+    print("perimeter of the rectangle:",(2*(l+b)))
+def perimeter_c(r):
+    print("perimeter of the circle:",(2*pi*r)
+
+
+# In[4]:
+
+
+get_ipython().system('jupyter nbconvert --to python rec_module.ipynb')
+
+
+# In[ ]:
+
+
+
+
